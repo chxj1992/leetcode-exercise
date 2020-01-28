803a864005ca555e4464e32d14b700d26a1e4c01
chxj1992 <chxj1992@gmail.com>
2020-01-28 20:20:11 +0800

    MinStack

---
+++ 155_min_stack/README.md
@@ -0,0 +1,9 @@
+## 155. 最小栈
+
+https://leetcode-cn.com/problems/min-stack/
+
+
+#### 解法  
+
+* [辅助栈](_1.py)
+* [辅助栈(优化)](_2.py)
---
+++ 155_min_stack/_1.py
@@ -0,0 +1,58 @@
+import unittest
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.ordered = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        inserted = False
+        for i, t in enumerate(self.ordered):
+            if t > x:
+                self.ordered.insert(i, x)
+                inserted = True
+                break
+        if not inserted:
+            self.ordered.append(x)
+
+    def pop(self) -> None:
+        x = self.stack.pop()
+        self.ordered.pop(self.ordered.index(x))
+
+    def top(self) -> int:
+        return self.stack[- 1]
+
+    def getMin(self) -> int:
+        return self.ordered[0]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        minStack = MinStack()
+        minStack.push(-2)
+        minStack.push(0)
+        minStack.push(-3)
+        self.assertEqual(-3, minStack.getMin())
+        minStack.pop()
+        minStack.top()
+        self.assertEqual(0, minStack.top())
+        self.assertEqual(-2, minStack.getMin())
+
+
+if __name__ == '__main__':
+    unittest.main()
