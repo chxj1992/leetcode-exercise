248b620791611001ebb471dcf8284437264b2f20
chxj1992 <chxj1992@gmail.com>
2021-04-15 17:59:17 +0800

    save

--- interviews/binance/_1.py
+++ interviews/binance/_1.py
@@ -1,17 +1,28 @@
 import unittest
-from typing import List
 
 
 class Solution:
-    def solve(self, input: List[int]) -> bool:
-        return True
+    def sqrt(self, n: int) -> float:
+        x, start, end = n, 0, n
+        while abs(x * x - n) > 0.001:
+            x = (start + end) / 2
+            if x * x == n:
+                return x
+            elif x * x > n:
+                end = x
+            else:
+                start = x
+        return round(x * 1000) / 1000
 
 
 class Test(unittest.TestCase):
 
     def test(self):
         s = Solution()
-        self.assertEqual(True, s.solve([1, 2, 3]))
+        print(s.sqrt(1))
+        print(s.sqrt(2))
+        print(s.sqrt(4))
+        print(s.sqrt(9))
 
 
 if __name__ == '__main__':
